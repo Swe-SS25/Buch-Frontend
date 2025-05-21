4fd360ab0e2d520911b1365d423f8a7ed12d46cb
malikbredehoeft2 <malikbredehoeft@gmail.com>
2025-05-21 15:24:59 +0200

    feat: initialize apollo

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Provider } from "@/components/ui/provider"
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { Provider } from '@/components/ui/provider';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql', //TODO: An Backend anpassen
+  cache: new InMemoryCache(),
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider>
+        <App />
+      </Provider>
+    </ApolloProvider>
   </StrictMode>,
-)
+);
