f17b9f054caceaaf378fb9dd30843a8c51478671
malikbredehoeft2 <malikbredehoeft@gmail.com>
2025-06-11 13:28:32 +0200

    Revert "feat: initialize apollo"
    This reverts commit 4fd360ab0e2d520911b1365d423f8a7ed12d46cb.

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-import { Provider } from '@/components/ui/provider';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql', //TODO: An Backend anpassen
-  cache: new InMemoryCache(),
-})
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { Provider } from "@/components/ui/provider"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ApolloProvider client={client}>
-      <Provider>
-        <App />
-      </Provider>
-    </ApolloProvider>
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>,
-);
+)
