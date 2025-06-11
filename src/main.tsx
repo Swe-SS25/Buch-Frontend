e54727a83746c67f8d306975d1b5d3069c71a93a
Lukas Baldauf <lukas.baldauf02@gmx.de>
2025-06-11 15:33:50 +0200

    fix: username in, email out

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,16 @@ import './index.css';
 import App from './App.tsx';
 import { Provider } from '@/components/ui/provider';
 import { SearchCriteriaProvider } from './context/SearchCriteriaContext.tsx';
-
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter> {/* <-- Hier */}
+    <BrowserRouter> 
       <SearchCriteriaProvider>
         <Provider>
           <App />
         </Provider>
       </SearchCriteriaProvider>
-    </BrowserRouter> {/* <-- Und hier */}
+    </BrowserRouter>
   </StrictMode>,
 );
