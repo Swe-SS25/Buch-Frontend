aa9752cff9e0ed05d696fcaedeb1c695f27b2c0f
Lukas Baldauf <lukas.baldauf02@gmx.de>
2025-06-17 14:22:23 +0200

    feat: https + cert

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import fs from 'fs';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
     https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'cert/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert/cert.pem')),
+      key: fs.readFileSync("cert/key.pem"),
+      cert: fs.readFileSync("cert/cert.pem"),
     },
     proxy: {
       '/graphql': {
