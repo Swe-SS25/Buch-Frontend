1bde76c2089ffbae6c4a7775233ad906013674aa
Lukas Baldauf <lukas.baldauf02@gmx.de>
2025-06-20 17:11:15 +0200

    fix: function expression

--- src/pages/login/page.tsx
+++ src/pages/login/page.tsx
@@ -1,6 +1,6 @@
 import LoginForm from '@/components/login/loginForm';
 
-export default function Login() {
+const Login = () => {
   return (
     <main className="d-flex justify-content-center align-items-center vh-100">
       <div className="card p-4 shadow-sm">
@@ -8,4 +8,6 @@ export default function Login() {
       </div>
     </main>
   );
-}
+};
+
+export default Login;
